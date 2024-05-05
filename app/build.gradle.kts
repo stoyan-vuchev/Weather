0999701f7866d165d76fa57e734ea378be44a024
Stoyan Vuchev <stoyan.v.vuchev@gmail.com>
2024-05-06 00:35:15 +0300

    Initial commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,185 @@
+import com.android.build.gradle.internal.cxx.configure.gradleLocalProperties
+
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.jetbrains.kotlin.android)
+    alias(libs.plugins.ksp)
+    alias(libs.plugins.daggerHilt.android.gradle)
+}
+
+android {
+
+    namespace = "com.stoyanvuchev.weather"
+    compileSdk = 34
+
+    signingConfigs {
+
+        getByName("debug") {
+            storeFile = getFile("keystore.path")
+            storePassword = getProperty("keystore.password")
+            keyAlias = getProperty("key.alias")
+            keyPassword = getProperty("key.password")
+        }
+
+        create("release") {
+            storeFile = getFile("keystore.path")
+            storePassword = getProperty("keystore.password")
+            keyAlias = getProperty("key.alias")
+            keyPassword = getProperty("key.password")
+        }
+
+    }
+
+    defaultConfig {
+
+        applicationId = "com.stoyanvuchev.weather"
+        minSdk = 27
+        targetSdk = 34
+
+        versionCode = 1
+        versionName = "0.0.24.5.5.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        vectorDrawables.useSupportLibrary = true
+
+    }
+
+    buildTypes {
+
+        debug {
+
+            isMinifyEnabled = false
+            isShrinkResources = false
+            isDebuggable = true
+            signingConfig = signingConfigs.getByName("debug")
+
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+
+        }
+
+        release {
+
+            isMinifyEnabled = true
+            isShrinkResources = true
+            isDebuggable = false
+            signingConfig = signingConfigs.getByName("release")
+
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+
+        }
+
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+
+    buildFeatures {
+        compose = true
+    }
+
+    composeOptions {
+        kotlinCompilerExtensionVersion = extra["compose.compiler.version"] as String
+    }
+
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+
+}
+
+dependencies {
+
+    // Core Dependencies
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.work)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.lifecycle.runtime.compose)
+    implementation(libs.androidx.activity.compose)
+    implementation(libs.androidx.navigation.compose)
+    implementation(libs.materialDesign)
+
+    testImplementation(libs.junit)
+
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+
+    // Compose Dependencies
+
+    implementation(platform(libs.compose.bom))
+    implementation(libs.compose.runtime)
+    implementation(libs.compose.foundation)
+    implementation(libs.compose.animation)
+    implementation(libs.compose.ui)
+    implementation(libs.compose.ui.graphics)
+    implementation(libs.compose.ui.tooling.preview)
+    implementation(libs.compose.material3)
+    implementation(libs.compose.material3.windowSizeClass)
+
+    debugImplementation(platform(libs.compose.bom))
+    debugImplementation(libs.compose.ui.tooling)
+    debugImplementation(libs.compose.ui.test.manifest)
+
+    androidTestImplementation(platform(libs.compose.bom))
+    androidTestImplementation(libs.compose.ui.test.junit4)
+
+    // Look & Feel Dependencies
+
+    implementation(libs.systemUiBarsTweaker)
+    implementation(libs.squircleShape)
+    implementation(libs.responsiveScaffold)
+
+    // Local Storage Dependencies
+
+    implementation(libs.datastore.preferences)
+    implementation(libs.room.runtime)
+    implementation(libs.room.ktx)
+    ksp(libs.room.compiler)
+
+    // Dependency Injection Dependencies
+
+    implementation(libs.daggerHilt.android)
+    ksp(libs.daggerHilt.compiler)
+
+    androidTestImplementation(libs.daggerHilt.android.testing)
+
+    implementation(libs.androidx.hilt.navigation.compose)
+    implementation(libs.androidx.hilt.work)
+    ksp(libs.androidx.hilt.compiler)
+
+    // Widget Dependencies
+
+    implementation(libs.glance)
+    implementation(libs.glance.appwidget)
+    implementation(libs.glance.tools.appwidget.host)
+    implementation(libs.glance.tools.appwidget.configuration)
+
+    // Testing Dependencies
+
+    testImplementation(libs.assertK)
+    testImplementation(libs.appCash.turbine)
+    testImplementation(libs.coroutinesTest)
+
+}
+
+ksp {
+    arg("room.schemaLocation", "$projectDir/schemas")
+}
+
+private fun getFile(path: String): File = file(getProperty(path))
+private fun getProperty(name: String): String =
+    gradleLocalProperties(rootDir, providers).getProperty(name)
---
+++ app/src/main/java/com/stoyanvuchev/weather/MainActivity.kt
@@ -0,0 +1,59 @@
+package com.stoyanvuchev.weather
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.stoyanvuchev.weather.ui.theme.WeatherTheme
+import dagger.hilt.android.AndroidEntryPoint
+import sv.lib.squircleshape.SquircleShape
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            WeatherTheme {
+
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+
+                    Box(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(innerPadding),
+                        contentAlignment = Alignment.Center
+                    ) {
+
+                        Text(
+                            modifier = Modifier
+                                .border(
+                                    width = 1.dp,
+                                    color = MaterialTheme.colorScheme.outlineVariant,
+                                    shape = SquircleShape()
+                                )
+                                .padding(horizontal = 12.dp, vertical = 6.dp),
+                            text = "Hello ${stringResource(id = R.string.app_name)}!"
+                        )
+
+                    }
+
+                }
+
+            }
+        }
+    }
+
+}
